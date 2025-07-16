7d602dfc07bba7feac70077d9d059bc64d7bb5ef
Pimma783 <fao12fao@gmail.com>
2025-07-16 23:59:59 +0700

    add link page

---
+++ Java/login.js
@@ -0,0 +1,13 @@
+function Login(event) {
+  event.preventDefault(); // ป้องกันฟอร์มส่งเอง
+
+  const userType = document.getElementById('userType').value;
+
+  if (userType === 'student') {
+    window.location.href = 'student.html';
+  } else if (userType === 'teacher') {
+    window.location.href = 'teacherpage.html';
+  } else {
+    alert('กรุณาเลือกประเภทผู้ใช้');
+  }
+}
